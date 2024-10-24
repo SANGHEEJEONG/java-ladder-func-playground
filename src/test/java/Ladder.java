1196afa161c518697c84cad921856f802610bfaa
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2024-10-25 00:59:25 +0900

    [FEAT] Ladder & Line 클래스 생성

---
+++ src/test/java/Ladder.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Ladder {
+    private final List<Line> lines;
+
+    public Ladder(List<Line> lines) {
+        this.lines = lines;
+    }
+}
---
+++ src/test/java/Line.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Line {
+    private final List<Boolean> points;
+
+    public Line(List<Boolean> points) {
+        this.points = points;
+    }
+}
