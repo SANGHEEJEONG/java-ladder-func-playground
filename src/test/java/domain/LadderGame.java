07fe9759162e38a403a5fdb5423a81c795341691
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2024-10-26 13:13:30 +0900

    [FEAT] 사다리 게임 실행 메서드 작성 (3단계)

--- src/test/java/domain/LadderGame.java
+++ src/test/java/domain/LadderGame.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LadderGame {
@@ -14,4 +15,19 @@ public class LadderGame {
         return ladderOrder;
     }
 
+    private int moveEachLadder(Ladder ladder, int ladderOrder) {
+        int result = ladderOrder;
+        for (Line line : ladder.getLadder()) {
+            result = decideWhereToGo(line, result);
+        }
+        return result;
+    }
+
+    public List<Integer> runGame(Ladder ladder, int width) {
+        List<Integer> result = new ArrayList<>(width);
+        for (int i = 0; i < width; i++) {
+            moveEachLadder(ladder, i);
+        }
+        return result;
+    }
 }
