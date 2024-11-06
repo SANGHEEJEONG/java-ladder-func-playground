beeb626c5278cac9a65cc61d0196bcbd0926b565
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2024-11-07 04:12:25 +0900

    [REFACTOR] 미사용 import 정리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,14 +1,4 @@
 import controller.LadderController;
-import domain.Ladder;
-import domain.LadderGame;
-import domain.LadderGenerator;
-import domain.Players;
-import domain.ResultTypes;
-import view.OutputView;
-import view.InputView;
-
-import java.util.List;
-
 
 public class Application {
     public static void main(String[] args) {
