7e4cd52399e9bd12293d0e6d8fbf3d14ee01997b
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2024-11-09 13:26:04 +0900

    [REFACTOR] LadderController 수정

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -11,39 +11,39 @@ import java.util.List;
 
 public class LadderController {
 
-    private Players players;
-    private Ladder ladder;
-
     public void run() {
         List<String> playerNames = InputView.splitString(InputView.inputNames());
         List<String> kindOfResults = InputView.splitString(InputView.inputLadderResults());
 
         // 플레이어 생성
-        players = new Players(playerNames);
+        Players players = new Players(playerNames);
         // 결과 종류 생성
         ResultTypes resultTypes = new ResultTypes(kindOfResults, players.getPlayersSize());
         // 사다리 초기화
-        initializeLadder();
+        Ladder ladder = initializeLadder(players);
 
         // 사다리 및 결과 출력
-        displayLadder(resultTypes, playerNames);
-        playGameAndDisplayResults(resultTypes);
+        displayLadder(resultTypes, playerNames, ladder);
+        playGameAndDisplayResults(resultTypes, players, ladder);
     }
 
-    private void initializeLadder() {
+    private Ladder initializeLadder(Players players) {
         int width = players.getPlayersSize();
         int height = InputView.inputHeight();
+
         LadderGenerator ladderGenerator = new LadderGenerator();
-        ladder = ladderGenerator.createLadder(width, height);
+        Ladder ladder = ladderGenerator.createLadder(width, height);
+
+        return ladder;
     }
 
-    private void displayLadder(ResultTypes resultTypes, List<String> playerNames) {
+    private void displayLadder(ResultTypes resultTypes, List<String> playerNames, Ladder ladder) {
         OutputView.printPlayers(playerNames);
         OutputView.drawLadder(ladder);
         OutputView.printResultTypes(resultTypes.getResultTypes());
     }
 
-    private void playGameAndDisplayResults(ResultTypes resultTypes) {
+    private void playGameAndDisplayResults(ResultTypes resultTypes, Players players, Ladder ladder) {
         players.moveAllPlayers(ladder);
         OutputView.printResult(players, resultTypes.getResultTypes());
     }
