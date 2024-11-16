ac02d9c4f80f41f3ad292168f0d8b31f47ebf2dc
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2024-11-17 00:43:47 +0900

    [REFACTOR] moveLeft & moveRight 예외 처리

--- src/main/java/domain/Position.java
+++ src/main/java/domain/Position.java
@@ -20,15 +20,17 @@ public class Position {
     }
 
     public void moveLeft() {
-        if (position != 0) {
-            position--;
+        if (position == 0){
+            throw new IllegalStateException("가장 왼쪽 사다리이므로 왼쪽으로 이동이 불가합니다.");
         }
+        position--;
     }
 
     public void moveRight(int maxPosition) {
-        if (position != maxPosition) {
-            position++;
+        if (position == maxPosition){
+            throw new IllegalStateException("가장 오른쪽 사다리이므로 오른쪽으로 이동이 불가합니다.");
         }
+        position++;
     }
 
     public int getPosition() {
