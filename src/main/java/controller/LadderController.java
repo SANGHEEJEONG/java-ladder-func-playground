d48bce2aec899fd76d541191b0e682468f17dcd5
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2024-11-19 02:53:39 +0900

    [REFACTOR] Controller 와 Application 책임 분리

--- src/main/java/controller/LadderController.java
+++ src/main/java/controller/LadderController.java
@@ -10,35 +10,25 @@ import java.util.List;
 
 public class LadderController {
 
-    public void run() {
-        Players players = initializePlayers();
-        ResultTypes resultTypes = initializeResults(players.getPlayersSize());
-        Ladder ladder = initializeLadder(players.getPlayersSize());
-
-        playGame(players, ladder);
-
-        displayAllOutput(players, resultTypes, ladder);
-    }
-
-    private Players initializePlayers() {
+    public Players initializePlayers() {
         List<String> playerNames = InputView.splitString(InputView.inputNames());
 
         return new Players(playerNames);
     }
 
-    private ResultTypes initializeResults(int resultsSize) {
+    public ResultTypes initializeResults(int resultsSize) {
         List<String> kindOfResults = InputView.splitString(InputView.inputLadderResults());
 
         return new ResultTypes(kindOfResults, resultsSize);
     }
 
-    private Ladder initializeLadder(int width) {
+    public Ladder initializeLadder(int width) {
         int height = InputView.inputHeight();
 
         return new Ladder(width, height);
     }
 
-    private void playGame(Players players, Ladder ladder) {
+    public void playGame(Players players, Ladder ladder) {
         players.moveAllPlayers(ladder);
     }
 
